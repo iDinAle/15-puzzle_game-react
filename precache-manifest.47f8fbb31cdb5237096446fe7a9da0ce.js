d3543134d165e5820ec387a2deb49e22dc8df692
Alexii Aidinov <alexii.aidinov@gmail.com>
2019-12-19 00:25:35 +0200

    Updates

---
+++ precache-manifest.47f8fbb31cdb5237096446fe7a9da0ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "261707966fd4d8a4f97c41a19931c844",
+    "url": "/15-puzzle_game-react/index.html"
+  },
+  {
+    "revision": "c614c5eebb8e0567019e",
+    "url": "/15-puzzle_game-react/static/css/main.111a5afc.chunk.css"
+  },
+  {
+    "revision": "25394c36eb70fbd53b88",
+    "url": "/15-puzzle_game-react/static/js/2.76520e5e.chunk.js"
+  },
+  {
+    "revision": "c614c5eebb8e0567019e",
+    "url": "/15-puzzle_game-react/static/js/main.3021c5b4.chunk.js"
+  },
+  {
+    "revision": "a029bd489df3d4fb1015",
+    "url": "/15-puzzle_game-react/static/js/runtime~main.d08d542d.js"
+  },
+  {
+    "revision": "a05f1fb32996567bf53cc6d415cc5e55",
+    "url": "/15-puzzle_game-react/static/media/background.a05f1fb3.png"
+  }
+]);
